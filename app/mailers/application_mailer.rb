33be77aedc5936430ff2567e1463673d16ed42a0
TwinkleVerma <tverma@isystango.com>
2018-08-31 12:35:53 +0530

    change default host in application_mailer.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'twinkleverma19961@gmail.com'
-  Rails.application.routes.default_url_options[:host] = 'localhost'
-  Rails.application.routes.default_url_options[:port] = '3000'
+  Rails.application.routes.default_url_options[:host] = 'fathomless-meadow-72894.herokuapp.com'
+  # Rails.application.routes.default_url_options[:port] = '3000'
   layout 'mailer'
 end
