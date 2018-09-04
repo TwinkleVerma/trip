04239571af78612b6248d8720331cbafded03476
TwinkleVerma <tverma@isystango.com>
2018-09-04 19:38:39 +0530

    env change

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'twinkleverma19961@gmail.com'
   Rails.application.routes.default_url_options[:host] = 'quiet-cliffs-56953.herokuapp.com'
-  Rails.application.routes.default_url_options[:host] = 'localhost' if Rails.env.development? || Rails.env.production?
-  Rails.application.routes.default_url_options[:port] = '3000' if Rails.env.development? || Rails.env.production?
+  Rails.application.routes.default_url_options[:host] = 'localhost' if Rails.env.development?
+  Rails.application.routes.default_url_options[:port] = '3000' if Rails.env.development?
   layout 'mailer'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-if Rails.env.development? || Rails.env.production?
+if Rails.env.development?
   Rails.application.config.action_mailer.default_url_options = { :host => 'localhost', port: 3000 }
 else
   Rails.application.config.action_mailer.default_url_options = { :host => 'quiet-cliffs-56953.herokuapp.com'}
